31d5b1bcf70ea8faa4ee147e4d7cbe4c76e75364
Adam Noon <adam.noon@gettyimages.com>
2016-11-02 14:39:37 -0600

    Adding bash files

---
+++ .bash.d/display.bash
@@ -0,0 +1,1 @@
+alias setdisplay="xrandr --output HDMI2 --off --output HDMI3 --off && xrandr --output LVDS1 --primary --mode 1366x768 --pos 0x410 --output HDMI3 --mode 1920x1200 --pos 1366x0 --output HDMI2 --mode 1920x1200 --pos 3286x0"
